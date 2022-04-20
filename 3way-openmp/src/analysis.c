b7a6b549c29a0b031ff4cab13129ad0d2fe3b2f9
schoggatt <schoggatt@ksu.edu>
2022-04-20 15:11:57 -0500

    added directories for each section of the project

---
+++ 3way-openmp/src/analysis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "dyn_array.h"
+#include "processing_scheduling.h"
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
---
+++ 3way-openmp/src/ascii_min_chars.c
@@ -0,0 +1,5 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "processing_scheduling.h"
