aa975a0ae8a60a47083e8c5c22a7b6b0d6e58375
Cody Reeves <rcody720@eos.beocat.ksu.edu>
2022-04-26 15:29:42 -0500

    MPI ready for testing

--- 3way-mpi/mpi_min_ascii.c
+++ 3way-mpi/mpi_min_ascii.c
@@ -12,10 +12,11 @@ int get_file_size(char *filename);
 char find_line_min(char *line, int line_length)
 {
     int min = INT32_MAX;
-    //printf("This is the line: %s", line);
+    // printf("This is the line: %s", line);
     int i;
-    for (i = 0; i < line_length - 1; i++){
-        //printf("The character is: %d\n", (int)line[i]);
+    for (i = 0; i < line_length - 1; i++)
+    {
+        // printf("The character is: %d\n", (int)line[i]);
         if ((int)line[i] < min && (int)line[i] > 32) // this could be changed but I do not know if we ignore control characters
         {
             min = (int)line[i];
@@ -29,7 +30,7 @@ bool find_file_min_chars(int *result, char *filename, int total_lines) // add to
     FILE *file;
     char *line = (char *)malloc(2001);
 
-    //file = fopen("/homes/dan/625/wiki_dump.txt", "r");
+    // file = fopen("/homes/dan/625/wiki_dump.txt", "r");
     file = fopen(filename, "r");
 
     int line_number; // max line is 10 for right now to limit run time and ensure accuracy
@@ -44,7 +45,7 @@ bool find_file_min_chars(int *result, char *filename, int total_lines) // add to
         // if (err == EOF) // we cannot do this since openmp must not have a condition that can be broken upon parallel execution from my reading
         //     break;
         line_length = strlen(line);
-        // printf("the line is: %s, The length is: %d\n", line, line_length); 
+        // printf("the line is: %s, The length is: %d\n", line, line_length);
         result[line_number] = (int)(find_line_min(line, line_length));
         // printf("%d\n", result[line_number]);
         line_number++;
@@ -54,24 +55,26 @@ bool find_file_min_chars(int *result, char *filename, int total_lines) // add to
     return true;
 }
 
-main(int argc, char* argv[]) 
+main(int argc, char *argv[])
 {
     int i, rc;
     int numtasks, rank;
     MPI_Status Status;
 
-    rc = MPI_Init(&argc,&argv);
-    if (rc != MPI_SUCCESS) {
-      printf ("Error starting MPI program. Terminating.\n");
-          MPI_Abort(MPI_COMM_WORLD, rc);
-        }
+    rc = MPI_Init(&argc, &argv);
+    if (rc != MPI_SUCCESS)
+    {
+        printf("Error starting MPI program. Terminating.\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+    }
 
-        MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     NUM_THREADS = numtasks;
 
-    if ( rank == 0 ) {
+    if (rank == 0)
+    {
         int file = open(FILE_PATH, O_RDONLY);
 
         int file_size = get_file_size(FILE_PATH);
@@ -88,7 +91,7 @@ main(int argc, char* argv[])
         lines[0] = line;
 
         int total_lines = 1;
-        while((line = strtok_r(NULL, "\n", &buffer_start)))
+        while ((line = strtok_r(NULL, "\n", &buffer_start)))
         {
             lines[total_lines] = line;
             total_lines++;
@@ -97,15 +100,23 @@ main(int argc, char* argv[])
         int result[total_lines];
     }
 
-    MPI_Bcast(char_array, ARRAY_SIZE * STRING_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+    // Might need to multiply total_lines by the size of each line
+    MPI_Bcast(buffer_start, total_lines, *MPI_CHAR, 0, MPI_COMM_WORLD);
 
     find_file_min_chars(result, lines, total_lines);
 
-    MPI_Reduce(local_char_count, char_counts, ALPHABET_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // The second parameter might not be right? It is optional so we may not even need it
+    MPI_Reduce(result, buffer_start, total_lines, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 
+    if (rank == 0)
+    {
+        int i;
+        for (i = 0; i < total_lines; i++)
+        {
+            printf("%d: %d\n", i, result[i]);
+        }
 
-    if ( rank == 0 ) {
-        print_results();
+        close(file);
     }
 
     MPI_Finalize();
